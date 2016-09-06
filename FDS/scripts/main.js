c9e34a7106983472b5f080ae52eeae5a9bcc5dae
felixdies <treejohn@uos.ac.kr>
2016-09-06 23:25:42 +0900

    view 2D 3D graphs together

--- FDS/scripts/main.js
+++ FDS/scripts/main.js
@@ -1,6 +1,9 @@
 ﻿$(document).ready(function () {
     var graphData = new GraphData(mockupColumnNames, mockupData, 20);
 
-    var g1 = new Graph3D("divGraphs", graphData, 0, 1);
+    var g1 = new Graph2D("divGraph2D", graphData, 4, 1);
     g1.draw();
+
+    var g2 = new Graph3D("divGraph3D", graphData, 2, 1);
+    g2.draw();
 });
